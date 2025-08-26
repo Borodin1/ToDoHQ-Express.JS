9548143e6b1332b52e60eb69fe5f82e7e827c2af
Alexander Borodin <borodinag1902@gmail.com>
2025-08-26 22:32:51 +0300

    fix host

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 const appConfig = {
-  host: process.env.APP_HOST || "localhost",
+  host: process.env.PORT ? "0.0.0.0" : "localhost",
   port: process.env.PORT ? parseInt(process.env.PORT) : 1337,
 };
 
