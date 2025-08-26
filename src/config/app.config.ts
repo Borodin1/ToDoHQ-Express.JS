b260788cc28ec39d970a3c71a9de1f7620956538
Alexander Borodin <borodinag1902@gmail.com>
2025-08-26 22:27:07 +0300

    fix for deploy Render

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 const appConfig = {
-    host: process.env.APP_HOST as string,
-    port: parseInt(process.env.APP_PORT as string)
-}
+  host: process.env.APP_HOST || "localhost",
+  port: process.env.PORT ? parseInt(process.env.PORT) : 1337,
+};
 
 export default appConfig;
